b639dc00e683c776bfbed24a945b4792ca2cb154
pkarira <pulkit5208@gmail.com>
2016-05-18 00:10:32 +0530

    connecting thread made

--- app/src/main/java/com/example/thispc/mobilequiz/Connecting.java
+++ app/src/main/java/com/example/thispc/mobilequiz/Connecting.java
@@ -9,20 +9,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.UUID;
 
+
 public class Connecting extends AppCompatActivity {
-    Button btn;
-    EditText name;
+
 
     boolean refreshEnabled = false;
-
+    ConnectingThread ct = null;
     private BluetoothAdapter bluetoothAdapter;
     private ListView listview;
     private ArrayAdapter adapter;
@@ -30,14 +30,12 @@ public class Connecting extends AppCompatActivity {
 
 
 
-
+    private final static UUID uuid = UUID.fromString("fc5ffc49-00e3-4c8b-9cf1-6b72aad1001a");
     private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 BluetoothDevice bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-
                 adapter.add(bluetoothDevice.getName() + "\n" +bluetoothDevice.getAddress() );
             }
         }
@@ -57,11 +55,64 @@ public class Connecting extends AppCompatActivity {
             startActivityForResult(enableBluetoothIntent, ENABLE_BT_REQUEST_CODE);
             Toast.makeText(Connecting.this, "Bluetooth Enabled", Toast.LENGTH_SHORT).show();
         } else if (refreshEnabled) {
-            btn.setText("Find Opponent");
+
             refreshEnabled = false;
             adapter.clear();
             bluetoothAdapter.disable();
         }
-    }
-}
 
+
+        listview = (ListView) findViewById(R.id.listView);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemValue = (String) listview.getItemAtPosition(position);
+                String MAC = itemValue.substring(itemValue.length() - 17);
+                BluetoothDevice bluetoothDevice = bluetoothAdapter.getRemoteDevice(MAC);
+                String s1 = bluetoothAdapter.getAddress();
+                String s2 = "b7" + s1.substring(0, 1) + "46a40-c758-4868-aa19-7ac6b3475dfc";
+                ct = new ConnectingThread(bluetoothDevice, UUID.fromString(s2));
+                ct.start();
+            }
+        });
+    }
+private class ConnectingThread extends Thread {
+    private final BluetoothDevice bluetoothDevice;
+    private final BluetoothSocket bluetoothSocket;
+
+    public ConnectingThread(BluetoothDevice device, UUID u) {
+
+        BluetoothSocket temp = null;
+        bluetoothDevice = device;
+
+
+        try {
+            temp = bluetoothDevice.createRfcommSocketToServiceRecord(u);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        bluetoothSocket = temp;
+    }
+
+    public void run() {
+        bluetoothAdapter.cancelDiscovery();
+
+        try {
+            bluetoothSocket.connect();
+        } catch (IOException connectException) {
+            connectException.printStackTrace();
+            try {
+                bluetoothSocket.close();
+            } catch (IOException closeException) {
+                closeException.printStackTrace();
+            }
+        }
+
+        if (bluetoothSocket != null && bluetoothDevice != null) {
+
+
+            
+        }
+
+    }
+}}
