f68f0f6e56cb9a9538bd4ae5beecadea7f9ee9eb
pkarira <pulkit5208@gmail.com>
2016-05-30 16:11:41 +0530

    listining thread implemented

--- app/src/main/java/com/example/thispc/mobilequiz/Server.java
+++ app/src/main/java/com/example/thispc/mobilequiz/Server.java
@@ -1,11 +1,14 @@
 package com.example.thispc.mobilequiz;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +18,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -33,7 +37,25 @@ public class Server extends AppCompatActivity {
     private static final int ENABLE_BT_REQUEST_CODE = 1;
     private static final int DISCOVERABLE_BT_REQUEST_CODE = 2;
     private ArrayList<UUID> mUuids;
+    public static String MyName = "";
+    public static String OpponentName;
+    Button btn;
+    EditText name;
+    int c;
 
+    boolean refreshEnabled = false;
+
+    private BluetoothAdapter bluetoothAdapter;
+    private ListView listview;
+    private ArrayAdapter adapter;
+    private static final int ENABLE_BT_REQUEST_CODE = 1;
+    private static final int DISCOVERABLE_BT_REQUEST_CODE = 2;
+    private ArrayList<UUID> mUuids;
+    private static final int Finished_Activity = 3;
+    private static final int DISCOVERABLE_DURATION = 300;
+    public static BluetoothDevice mBluetoothDevice = null;
+    public static BluetoothSocket mBluetoothSocket = null;
+    ListeningThread t = null;
 
 
 
@@ -57,22 +79,141 @@ public class Server extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server);
         mUuids = new ArrayList<UUID>();
-        c=0;
+        c = 0;
         setContentView(R.layout.activity_main);
 
-                            if (bluetoothAdapter == null) {
-                                Toast.makeText(getApplicationContext(), "Device does not support Bluetooth",
-                                        Toast.LENGTH_SHORT).show();
-                            } else if (!refreshEnabled) {
-                                refreshEnabled = true;
-                                Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                                startActivityForResult(enableBluetoothIntent, ENABLE_BT_REQUEST_CODE);
-                                Toast.makeText(Server.this, "Bluetooth Enabled", Toast.LENGTH_SHORT).show();
-                            } else if (refreshEnabled) {
-                                refreshEnabled = false;
-                                adapter.clear();
-                                bluetoothAdapter.disable();
-                            }
-                        }
-                    }
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Device does not support Bluetooth",
+                    Toast.LENGTH_SHORT).show();
+        } else if (!refreshEnabled) {
+            refreshEnabled = true;
+            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetoothIntent, ENABLE_BT_REQUEST_CODE);
+            Toast.makeText(Server.this, "Bluetooth Enabled", Toast.LENGTH_SHORT).show();
+        } else if (refreshEnabled) {
+            refreshEnabled = false;
+            adapter.clear();
+            bluetoothAdapter.disable();
+        }
 
+
+        listview = (ListView) findViewById(R.id.listView);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1);
+        listview.setAdapter(adapter);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.disable();
+            adapter.clear();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == ENABLE_BT_REQUEST_CODE) {
+            // Bluetooth successfully enabled!
+            if (resultCode == Activity.RESULT_OK) {
+                Toast.makeText(getApplicationContext(), "Bluetooth enabled." + "\n" + "Scanning for peers", Toast.LENGTH_SHORT).show();
+
+                makeDiscoverable();
+                discoverDevices();
+                for (int i = 0; i < mUuids.size(); i++) {
+                    t = new ListeningThread(mUuids.get(i));
+                    t.start();
+
+
+
+                } }else {
+                Toast.makeText(getApplicationContext(), "Bluetooth is not enabled.", Toast.LENGTH_SHORT).show();
+            }
+        } else if (requestCode == DISCOVERABLE_BT_REQUEST_CODE) {
+            if (resultCode == DISCOVERABLE_DURATION) {
+                Toast.makeText(getApplicationContext(), "Your device is now discoverable for " + DISCOVERABLE_DURATION + " seconds", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Fail to enable discoverable mode.", Toast.LENGTH_SHORT).show();
+            }
+        } else if (resultCode == Finished_Activity) {
+            bluetoothAdapter.disable();
+            adapter.clear();
+            refreshEnabled = false;
+            btn.setText("Find Opponent");
+        }
+    }
+
+    protected void discoverDevices() {
+        if (bluetoothAdapter.startDiscovery()) {
+            Toast.makeText(getApplicationContext(), "Discovering peers", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Discovery failed to start.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void makeDiscoverable() {
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, DISCOVERABLE_DURATION);
+        startActivityForResult(discoverableIntent, DISCOVERABLE_BT_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.registerReceiver(broadcastReceiver, filter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.unregisterReceiver(broadcastReceiver);}
+
+    private class ListeningThread extends Thread {
+        BluetoothServerSocket bluetoothServerSocket;
+
+        public ListeningThread(UUID uuid) {
+
+            BluetoothServerSocket temp = null;
+            try {
+                temp = bluetoothAdapter.listenUsingRfcommWithServiceRecord(getString(R.string.app_name), uuid);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            bluetoothServerSocket = temp;
+
+        }
+
+        public void run() {
+            BluetoothSocket bluetoothSocket=null;
+
+
+            while (true) {
+                try {
+                    bluetoothSocket = bluetoothServerSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+                if (bluetoothSocket != null) {
+                    //connected(bluetoothSocket, bluetoothSocket.getRemoteDevice());
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            c++;
+                            Toast.makeText(getApplicationContext(), "A connection has been accepted."+c, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+            }
+
+
+            try {
+                bluetoothServerSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+
+}
