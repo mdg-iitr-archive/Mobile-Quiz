dc7979ec0bd5fd6befc1928c1f09512bcda308d2
pkarira <pulkit5208@gmail.com>
2016-05-30 16:20:34 +0530

    broadcast receiver in twodevices

--- app/src/main/java/com/example/thispc/mobilequiz/Twodevices.java
+++ app/src/main/java/com/example/thispc/mobilequiz/Twodevices.java
@@ -1,13 +1,48 @@
 package com.example.thispc.mobilequiz;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class Twodevices extends AppCompatActivity {
+
+    boolean refreshEnabled = false;
+
+    private BluetoothAdapter bluetoothAdapter;
+
+    private ArrayAdapter adapter;
+
+    private static final int DISCOVERABLE_DURATION = 300;
+
+
+
+     private final static UUID uuid = UUID.fromString("fc5ffc49-00e3-4c8b-9cf1-6b72aad1001a");
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                adapter.add(bluetoothDevice.getName() + "\n" + bluetoothDevice.getAddress());
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twodevices);
+
     }
 }
